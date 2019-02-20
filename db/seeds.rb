c835f5255c4d499727c9d7cc093322503fd7b537
Catherine Spyropoulos <cath.spyropoulos@gmail.com>
2019-02-20 15:45:15 +0100

    database migrated and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Large Flat Amsterdam',
+  address: '24 Korte Prinsengracht',
+  description: 'Amazing spot on the canals',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Trendy Barcelona house',
+  address: 'Gothic Quarter',
+  description: 'Perfect spot for a couple wanting to be in the heart of the city.',
+  price_per_night: 125,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Maisonette in Paris',
+  address: 'St Germaine',
+  description: 'Modern apartment in trendy St Germaine area',
+  price_per_night: 150,
+  number_of_guests: 4
+)
